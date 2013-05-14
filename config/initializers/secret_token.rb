232218575a9c65a70f1a5932a6f675e459ac59fc
Irfan Shah <mail.urfi@gmail.com>
2013-05-14 20:12:10 +0530

    sample app demonstrating debug assets problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRails3JasmineRails::Application.config.secret_token = 'd2c403e8cd6dc987ff48d0baa36feb65e4b79651f2f906d41ab5589426d18d30b5e3afc239caae8f022cb1e33673d2559621fdb26706b552fd73842d532c82a7'
